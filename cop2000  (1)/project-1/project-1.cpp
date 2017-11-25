9c8d6bf7574594d11d5a8abc7851a7306c3c12db
Rebecca <31871431+BeccaFlake@users.noreply.github.com>
2017-11-25 16:22:19 -0500

    Add files via upload

---
+++ cop2000  (1)/project-1/project-1.cpp
@@ -0,0 +1,19 @@
+//Rebecca Flake
+//COP2000.0M3
+//This program will print my personal information on four lines, using a single cout statement
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//program code goes here
+
+	cout <<"Rebecca Flake\n"
+		 <<"129 Stratford Mill Blvd Unit 103, Saint Augustine, FL 32084\n"
+		 <<"(904) 687-9246\n"
+		 <<"A.S. in Programming and Analysis with a B.A. in Linguistics\n\n";
+
+	return 0;
+
+}
